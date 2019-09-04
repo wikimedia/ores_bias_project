bdd56dc1289b9cf84fbfa8c3df255344837d6681
groceryheist <nathante@uw.edu>
2019-09-04 16:44:11 -0700

    add more tests

--- test_ores_archaeologist.sh
+++ test_ores_archaeologist.sh
@@ -3,10 +3,10 @@ cd ~/ores_bias/ores_bias_project
 source ../mediawiki-services-ores-deploy/bin/activate
 python3 ores_archaeologist.py get_threshhold --wiki_db=enwiki --date=2018-08-07 --threshhold_string="maximum recall @ precision >= 0.99"
 
-# python3 ores_archaeologist.py score_wiki_commit_revisions --commit=8c4ed2536dd668f909b48a75ff0688fdc481be09 --wiki_db=kowiki --all_revisions='ores_bias_data/test_revisions_sample.csv' --load-environment=False --wrap=True
+python3 ores_archaeologist.py score_wiki_commit_revisions --commit=8c4ed2536dd668f909b48a75ff0688fdc481be09 --wiki_db=kowiki --all_revisions='ores_bias_data/test_revisions_sample.csv' --load-environment=False --wrap=True
 
-# python3 ores_archaeologist.py score_commit_revisions --commit=8c4ed2536dd668f909b48a75ff0688fdc481be09 --cutoff_revisions='ores_bias_data/test_revisions_sample.csv' --load_environment=False --wrap=True
+python3 ores_archaeologist.py score_commit_revisions --commit=8c4ed2536dd668f909b48a75ff0688fdc481be09 --cutoff_revisions='ores_bias_data/test_revisions_sample.csv' --load_environment=False --wrap=True
 
-# python3 ores_archaeologist.py score_history --cutoff_revisions='ores_bias_data/test_revisions_sample.csv'  --wrap=True
+python3 ores_archaeologist.py score_history --cutoff_revisions='ores_bias_data/test_revisions_sample.csv'  --wrap=True
 
 # python3 ores_archaeologist.py score_revisions --wiki_db=enwiki --uri=https://en.wikipedia.org --date=2018-08-22 --infile=test_revids.txt
