f000462cd2808ff00b82deb5735cda532142d6e1
groceryheist <nathante@uw.edu>
2019-09-09 22:23:47 -0700

    add script to score the entire sample

---
+++ score_sample.sh
@@ -0,0 +1,3 @@
+:q#!/bin/bash
+source ./bin/activate
+python3 ores_archaeologist.py score_history --cutoff_revisions=ores_bias_data/cutoff_revisions_sample.csv --wrap=False --output=ores_bias_data/historically_scored_sample.csv
