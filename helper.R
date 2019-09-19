3f136c217489719fa41aba2598a0007083c809bf
groceryheist <nathante@uw.edu>
2019-09-19 11:01:59 -0700

    scale density to fit histogram

--- helper.R
+++ helper.R
@@ -70,6 +70,12 @@ ttr_grid_plot <- function(sample, wiki, tool, bins=50){
     hist.data <- gen.ttr.histogram.data(s, tool, bins)
     density.data <- gen.ttr.density.data(s, tool)
 
+    ## scale the density so it's max is the same as the max of the histogram
+    max.counts.hist <- hist.data[,.(max.hist=max(value)),by=.(variable)]
+    max.counts.dens <- density.data[,.(max.dense=max(value)),by=.(variable)]
+    scaling <- merge(max.counts.dens, max.counts.hist, by='variable')
+    density.data <- merge(density.data, scaling, by='variable',how='leftouter')
+    density.data <- density.data[,value := value * scaling.factor]
     p <- ggplot(hist.data) + geom_rect(data=hist.data, mapping=aes(xmin=xmin,xmax=xmax,ymin=0,ymax=value)) + geom_line(data=density.data, mapping=aes(x=x,y=value)) + facet_grid(cols=vars(revert_tool), rows=vars(variable), scales='free_y')
 
     p <- p + scale_x_continuous(breaks = log10(duration_x_values), labels = duration_x_names) + theme(axis.text.x=element_text(angle=90,hjust=1))
@@ -98,7 +104,7 @@ gen.ttr.histogram.data <- function(s, tool, bins){
 }
 
 gen.ttr.density.data <- function(s, tool){
-    p <- ggplot(s, aes_string(x=x,y=y)) + facet_grid(as.formula(paste0(facet_row, "~", facet_col))) + geom_density() + scale_x_log10(breaks=breaks, labels=labels)
+    p <- ggplot(s, aes_string(x=x,y='..count..')) + facet_grid(as.formula(paste0(facet_row, "~", facet_col))) + geom_density() + scale_x_log10(breaks=breaks, labels=labels)
 
     p1_data <- data.table(ggplot_build(p)$data[[1]])
     
