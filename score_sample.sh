42848fdd4afd99972bda8b4349ced9b6da7066ab
groceryheist <groceryheist@gmail.com>
2019-10-16 01:18:22 +0000

    update test

--- score_sample.sh
+++ score_sample.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source ./bin/activate
-python3 ores_archaeologist.py score_history --cutoff_revisions=ores_bias_data/cutoff_revisions_sample.csv --wrap=False --output=ores_bias_data/historically_scored_sample.csv > output 2> error
+python3 ores_archaeologist.py score_history --cutoff_revisions=data/enwiki_revisions_small.csv --wrap=False --output=data/enwiki_scored_small_sample.csv > output 2> error
+#python3 ores_archaeologist.py score_history --cutoff_revisions=data/cutoff_revisions_sample.csv --wrap=False --output=data/historically_scored_sample.csv > output 2> error
