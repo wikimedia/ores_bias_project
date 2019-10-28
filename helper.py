a4683a8a39b7cfd9459f158adcb82dc2069be013
Nathan TeBlunthuis <nathante@uw.edu>
2019-10-28 12:48:46 -0700

    don't worry if we can't get revscoring versions from model info, just log it

--- helper.py
+++ helper.py
@@ -336,8 +336,14 @@ def set_revscoring_version(model_file, commit):
     # other wise pass --formatting=json
     # we might need to special case lvwiki
 
-    proc = subprocess.run(call, shell=True, executable="/bin/bash", stdout=subprocess.PIPE, universal_newlines=True)
-    res = json.loads(proc.stdout)
-    version = res.get("environment",{}).get("revscoring_version",None)
+    try:
+        proc = subprocess.run(call, shell=True, executable="/bin/bash", stdout=subprocess.PIPE, universal_newlines=True)
+        res = json.loads(proc.stdout)
+        version = res.get("environment",{}).get("revscoring_version",None)
+    except Exception e:
+        print(e)
+        print(model_file)
+        version = None
+
     if version is not None:
         subprocess.run("source ../mediawiki-services-ores-deploy/bin/activate && pip3 install revscoring=={0}".format(version), shell=True, executable="/bin/bash")
