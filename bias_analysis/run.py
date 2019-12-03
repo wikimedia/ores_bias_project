06c94c51a40a57d3d6da3e8ff201c22e2fa89f70
groceryheist <nathante@uw.edu>
2019-12-03 06:26:46 +0000

    refactor code for evaluating encoded bias

--- bias_analysis/run.py
+++ bias_analysis/run.py
@@ -10,10 +10,10 @@ wikis = list(load_wikis())
 makefile = load_makefile()
 label_files = list(map(lambda x: grep_labelfile(x, makefile), wikis))
 download_labels(label_files)
-score_labels(label_files, wikis)
+score_labels(label_files, wikis, overwrite=True)
 
 move_labels_to_datalake(label_files, wikis)
 
-subprocess.call(["spark-submit", "--name","get ores label user histories", "--master", "yarn", "--conf", "spark.driver.memory=4g","--conf", "spark.executor.memory=8g","--conf", "spark.executor.memoryOverhead=2g",  "get_label_user_histories.py"])
+subprocess.call(["/usr/lib/spark2/bin/spark-submit", "--name","get ores label user histories", "--master", "yarn", "--conf", "spark.driver.memory=4g","--conf", "spark.executor.memory=8g","--conf", "spark.executor.memoryOverhead=2g",  "get_label_user_histories.py"])
 
 subprocess.call("./evaluate_encoded_bias.py")
